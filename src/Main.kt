4007d3e66b93783356b76a184fb4ad0b599f2c93
waimea-snoble <snoble@waimea.school.nz>
2024-10-22 09:56:17 +1300

    first user turn

--- src/Main.kt
+++ src/Main.kt
@@ -28,18 +28,6 @@ fun main() {
     coins.add("X")
     coins.add("O")
 
-    // Highlighted change: Place coins in the grid before displaying it
-    for (coin in coins) {
-        var position: Int
-        do {
-            position = Random.nextInt(0, grid.size) // Get a random position from 0 to 19
-        } while (grid[position] != " ") // Ensure the position is empty
-        grid[position] = coin // Place the coin in the grid
-    }
-
-    // Highlighted change: Display the game board after placing coins
-    gameBoard(grid) // Now it will display the grid with coins
-
     // Get player names
     println("Enter the name of player 1")
     val name1 = readln()
@@ -47,19 +35,49 @@ fun main() {
     println("Enter the name of player 2")
     val name2 = readln()
 
-    println("Hello $name1 and $name2")
+    println("Hello $name1 and $name2 welcome to Old Gold")
+
+    // Highlighted change: Place coins in the grid before displaying it
+    for (coin in coins) {
+        var position: Int
+        do {
+            position = Random.nextInt(0, 20) // Get a random position from 0 to 20
+        } while (grid[position] != " ") // Ensure the position is empty
+        grid[position] = coin // Place the coin in the grid
+    }
+
+    // Highlighted change: Display the game board after placing coins
+    gameBoard(grid) // Now it will display the grid with coins
+
+    var currentPlayer = name1
+    println("$currentPlayer, select the number of the coin you would like to move")
+    val select = readln().toInt() - 1
+
+    if (grid[select] == " ") {
+        println("There is no coin in this space, try again")
+    }
+
 }
 
 /**
  * Function to display the game board
  */
 fun gameBoard(grid: MutableList<String>) {
-    println("+--------------------------------------------------------------+")
+    println("+-------------------------------------------------------------------------------+")
     // Print the grid with coins
     print("|")
     for (i in grid.indices) {
         print(" ${grid[i]} |") // Print each coin or empty space with borders
     }
     println() // New line after the row
-    println("+--------------------------------------------------------------+")
+    println("+-------------------------------------------------------------------------------+")
+
+    var index = 1
+    for (coin in grid) {
+        print("|%-2d ".format(index))
+        index++
+
+    }
+    print("|")
+    println() // New line after the numbers
 }
